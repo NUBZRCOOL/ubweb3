244563dc2ae4254a5c39869707448943fd3a1605
NUBZRCOOL <bhattinubaid@gmail.com>
2023-02-20 12:25:47 -0800

    Initial commit.

---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+
+    return (
+
+        <div className="App-header">
+
+            <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../static/css/App.css';
+
+const Home = () => {
+
+    return (
+
+        <div className="App-header">
+
+            <p>E</p>
+        </div>
+    )
+}
+
+export default Home;
